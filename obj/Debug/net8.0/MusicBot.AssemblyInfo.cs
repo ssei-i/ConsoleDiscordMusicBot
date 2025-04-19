8a210a9c2feb53192dc37d600e2620b35f71146b
ssei-i <jzselasor@gmail.com>
2025-04-19 19:08:39 +0800

    added the ability to replace individual value of the config

--- obj/Debug/net8.0/MusicBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/MusicBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ee241f34f4bc53f93d4c9593173526cd386d17b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
